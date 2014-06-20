8b1795fef513b89b37da3fb14674afd642efc96b
RifqiSec <rifqisec@live.com>
2014-06-20 13:59:50 +0700

    fix

--- application/views/side.php
+++ application/views/side.php
@@ -1,95 +1,95 @@
 <div class="page-sidebar navbar-collapse collapse">
          <ul class="page-sidebar-menu">
             <li>
-               <div class="sidebar-toggler hidden-xs"></div>
-            </li>
-            <li class="start">
-               <a class="ajaxify start" href="<?php echo site_url('dashboardController') ?>">
-               <i class="icon-home"></i> 
-               <span class="title">Dashboard</span>
-               <span class="selected"></span>
+         <div class="sidebar-toggler hidden-xs"></div>
+      </li>
+      <li class="start">
+         <a class="ajaxify start" href="<?php echo site_url('dashboardController') ?>">
+         <i class="icon-home"></i> 
+         <span class="title">Dashboard</span>
+         <span class="selected"></span>
+         </a>
+      </li>
+      <li class="">
+         <a class="ajaxify " href="<?php echo site_url('poController') ?>">
+         <i class="icon-shopping-cart"></i> 
+         <span class="title">Purchase Order</span>
+         <span class="selected"></span>
+         </a>
+      </li>
+      <li class="">
+         <a class="ajaxify " href="<?php echo site_url('kelas') ?>">
+         <i class="icon-search"></i> 
+         <span class="title">Review Purchase Order</span>
+         <span class="selected"></span>
+         </a>
+      </li>
+      <li class="">
+         <a class="ajaxify " href="<?php echo site_url('jurusan') ?>">
+         <i class="icon-tasks"></i> 
+         <span class="title">Manage Jurusan</span>
+         <span class="selected"></span>
+         </a>
+      </li>
+      <li >
+         <a href="javascript:;">
+         <i class="icon-cogs"></i> 
+         <span class="title">Pengaturan</span>
+         <span class="selected"></span>
+         <span class="arrow open"></span>
+         </a>
+         <ul class="sub-menu">
+            <li>
+               <a class="ajaxify" href="layout_ajax_content_2.html">
+               Ajax Link Sample 1                    
                </a>
             </li>
-            <li class="">
-               <a class="ajaxify " href="<?php echo site_url('poController') ?>">
-               <i class="icon-shopping-cart"></i> 
-               <span class="title">Purchase Order</span>
-               <span class="selected"></span>
+            <li>
+               <a class="ajaxify" href="layout_ajax_content_3.html">
+               Ajax Link Sample 2                    
                </a>
             </li>
-            <li class="">
-               <a class="ajaxify " href="<?php echo site_url('kelas') ?>">
-               <i class="icon-sitemap"></i> 
-               <span class="title">Review Purchase Order</span>
-               <span class="selected"></span>
+            <li>
+               <a class="ajaxify" href="layout_ajax_content_2.html">
+               Ajax Link Sample 3                    
                </a>
             </li>
-            <li class="">
-               <a class="ajaxify " href="<?php echo site_url('jurusan') ?>">
-               <i class="icon-tasks"></i> 
-               <span class="title">Manage Jurusan</span>
-               <span class="selected"></span>
+            <li>
+               <a class="ajaxify" href="layout_ajax_content_3.html">
+               Ajax Link Sample 4                    
                </a>
-            </li>
-            <li >
-               <a href="javascript:;">
-               <i class="icon-cogs"></i> 
-               <span class="title">Pengaturan</span>
-               <span class="selected"></span>
-               <span class="arrow open"></span>
-               </a>
-               <ul class="sub-menu">
-                  <li>
-                     <a class="ajaxify" href="layout_ajax_content_2.html">
-                     Ajax Link Sample 1                    
-                     </a>
-                  </li>
-                  <li>
-                     <a class="ajaxify" href="layout_ajax_content_3.html">
-                     Ajax Link Sample 2                    
-                     </a>
-                  </li>
-                  <li>
-                     <a class="ajaxify" href="layout_ajax_content_2.html">
-                     Ajax Link Sample 3                    
-                     </a>
-                  </li>
-                  <li>
-                     <a class="ajaxify" href="layout_ajax_content_3.html">
-                     Ajax Link Sample 4                    
-                     </a>
-                  </li>
-               </ul>
-            </li>
-            <li class="last">
-               <a href="javascript:;">
-               <i class="icon-cogs"></i> 
-               <span class="title">Report</span>
-               <span class="selected"></span>
-               <span class="arrow"></span>
-               </a>
-               <ul class="sub-menu">
-                  <li>
-                     <a class="ajaxify" href="layout_ajax_content_2.html">
-                     Ajax Link Sample 1                    
-                     </a>
-                  </li>
-                  <li>
-                     <a class="ajaxify" href="layout_ajax_content_3.html">
-                     Ajax Link Sample 2                    
-                     </a>
-                  </li>
-                  <li>
-                     <a class="ajaxify" href="layout_ajax_content_2.html">
-                     Ajax Link Sample 3                    
-                     </a>
-                  </li>
-                  <li>
-                     <a class="ajaxify" href="layout_ajax_content_3.html">
-                     Ajax Link Sample 4                    
-                     </a>
-                  </li>
-               </ul>
             </li>
          </ul>
-      </div>
+      </li>
+      <li class="last">
+         <a href="javascript:;">
+         <i class="icon-cogs"></i> 
+         <span class="title">Report</span>
+         <span class="selected"></span>
+         <span class="arrow"></span>
+         </a>
+         <ul class="sub-menu">
+            <li>
+               <a class="ajaxify" href="layout_ajax_content_2.html">
+               Ajax Link Sample 1                    
+               </a>
+            </li>
+            <li>
+               <a class="ajaxify" href="layout_ajax_content_3.html">
+               Ajax Link Sample 2                    
+               </a>
+            </li>
+            <li>
+               <a class="ajaxify" href="layout_ajax_content_2.html">
+               Ajax Link Sample 3                    
+               </a>
+            </li>
+            <li>
+               <a class="ajaxify" href="layout_ajax_content_3.html">
+               Ajax Link Sample 4                    
+               </a>
+            </li>
+         </ul>
+      </li>
+   </ul>
+</div>
